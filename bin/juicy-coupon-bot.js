838141dcce5f104e4ba5005ecde1b0a60031b732
Harsh Kumar <harshmastic@gmail.com>
2025-04-02 01:15:13 +0530

    lfkjakjfla'fjalfjl

--- bin/juicy-coupon-bot.js
+++ bin/juicy-coupon-bot.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-require('ts-node').register({ transpileOnly: true });
-require('../bin/juicy-coupon-bot.ts');
+import("tsx")
+  .then((tsx) => {
+    tsx.run("./juicy-coupon-bot.ts");
+  })
+  .catch((err) => {
+    console.error("Error loading tsx:", err);
+    process.exit(1);
+  });
